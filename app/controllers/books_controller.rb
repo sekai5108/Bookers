2a22c18146a4ff83a3e1f2aa03c9dd278c52c191
sekai5108 <sekai.5108@gmail.com>
2021-10-14 17:47:46 +0000

    Add 3-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
+
   end
 
   def new
@@ -9,12 +10,17 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book)
+    @book = Book.new(book_params)
+    @books = Book.all
+    if @book.save
+      redirect_to book_path(@book.id), notice: 'Book was successfully created.'
+    else
+      render :index
+    end
   end
 
   def edit
@@ -22,11 +28,19 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: 'Book was successfully update.'
+    else
+      render :edit
+    end
   end
 
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to book
+  end
 
   private
   def book_params
